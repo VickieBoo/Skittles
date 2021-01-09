79abfb1f70348709281048561763507d61c84c83
VickieBoo <sinfulodds@gmail.com>
2021-01-09 17:46:46 -0600

    Created "ColorGroup" class

--- Utilities/ColorGroup.cs
+++ Utilities/ColorGroup.cs
@@ -1,6 +1,12 @@
 // ReSharper disable CheckNamespace
+
+using System.Collections.Generic;
+using System.Drawing;
+
 namespace SkittlesPower {
     public class ColorGroup {
-        
+        public Color Parent;
+        public int Children;
+        public void Add() => Children += 1;
     }
 }
