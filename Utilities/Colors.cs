f9948fe4f425821ea600a5524d8f202bb38b75f7
VickieBoo <sinfulodds@gmail.com>
2021-01-09 17:46:26 -0600

    Created Color class extensions to use in "Smart" methods

--- Utilities/Colors.cs
+++ Utilities/Colors.cs
@@ -1,7 +1,31 @@
 // ReSharper disable CheckNamespace
 
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+
 namespace SkittlesPower {
     public static partial class Skittles {
-        // everything related to colors will go in here
+        public static string ToHex(this Color c, bool hash = true)
+            => $"{(hash ? "#" : "")}{c.R:X2}{c.G:X2}{c.B:X2}";
+        public static double Value(this Color c)
+            => int.Parse(c.ToHex(false), NumberStyles.HexNumber);
+        
+        /// <summary>
+        /// Finds the difference between 2 colors (needs improvements but has worked so far)
+        /// </summary>
+        /// <param name="c1">First color</param>
+        /// <param name="c2">Second color</param>
+        /// <returns></returns>
+        public static double Difference(this Color c1, Color c2) {
+            var values = new[] {c1.Value(), c2.Value()}.OrderByDescending(_=>_).ToArray();
+            var difference = (values[0] - values[1]) / values.Sum();
+            return difference switch {
+                double.NaN => -1, 
+                double.PositiveInfinity => -1, 
+                _ => difference
+            };
+        }
     }
 }
