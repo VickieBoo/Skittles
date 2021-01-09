5e1de943f2cb2f7f13e1c2e42a56019dd5de15cd
VickieBoo <sinfulodds@gmail.com>
2021-01-09 17:45:04 -0600

    Added "SkittlesApp" for testing purposes

---
+++ SkittlesApp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using SkittlesPower;
+
+namespace SkittlesApp {
+    internal class Program {
+        public static async Task Main(string[] args) {
+            var files = new[] {
+                "Images/pic1.jpeg", "Images/pic2.jpeg"
+            };
+            foreach (var file in files) {
+                if (!File.Exists(file)) continue;
+                var smart = await Skittles.SmartAsync(file);
+                Console.WriteLine($"-= Results for '{file}' =-");
+                Console.WriteLine($"Focused: {smart.Focused.ToHex()}");
+                Console.WriteLine($"Secondary: {smart.Secondary.ToHex()}");
+                Console.WriteLine($"Accent: {smart.Accent.ToHex()}");
+            }
+        }
+    }
+}
