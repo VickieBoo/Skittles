b901954fcdec606c779ec2ab4a8fc93b11db3c20
VickieBoo <sinfulodds@gmail.com>
2021-01-09 17:43:44 -0600

    Added tolerances

--- Tolerances.cs
+++ Tolerances.cs
@@ -1,5 +1,6 @@
 namespace SkittlesPower {
     public static class Tolerances {
-        // todo: add tolerance values
+        public static double Transparency = 255; // minimum alpha (transparency) allowed (0 full transparency allowed, 255 solid colors only)
+        public static double Similarity = 0.20; // how similar a color can be until it's considered a different color
     }
 }
