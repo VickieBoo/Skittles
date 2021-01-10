18db04c9653423aae3f3096a255c6f90e7372409
VickieBoo <sinfulodds@gmail.com>
2021-01-09 18:01:14 -0600

    Forgot to add these in the last push

--- Skittles/AverageAsync.cs
+++ Skittles/AverageAsync.cs
@@ -1,16 +1,37 @@
 // ReSharper disable CheckNamespace poggers
 
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace SkittlesPower {
     public static partial class Skittles {
-        
         /// <summary>
         /// Gets the average color of an image
         /// </summary>
         /// <param name="source">File path or URL</param>
         /// <param name="width">New width of the image</param>
         /// <param name="height">New height of the image</param>
-        public static void AverageAsync(string source, int width = 32, int height = 32) {
+        public static async Task<Color> AverageAsync(string source, int width = 32, int height = 32)
+            => Average(await GetImageAsync(source));
+        
+        public static Color Average(Bitmap image, int width = 32, int height = 32) {
+            image = (Bitmap)image.GetThumbnailImage(width, height, () => false, IntPtr.Zero); // resizes image
+            var colors = new List<Color>();
+            // first for loop goes down the image after the second for loop has finished going left to right
+            for (var h = 0; h < image.Height; h++) 
+                for (var w = 0; w < image.Width; w++) {
+                    var pixel = image.GetPixel(w, h);
+                    if (pixel.IsEmpty || pixel.A < Tolerances.Transparency) continue;
+                    colors.Add(pixel);
+                }
             
+            var R = colors.Select(color => (int)color.R).Sum() / colors.Count;
+            var G = colors.Select(color => (int)color.G).Sum() / colors.Count;
+            var B = colors.Select(color => (int)color.B).Sum() / colors.Count;
+            return Color.FromArgb(R, G, B);
         }
     }
 }
--- Skittles/CommonAsync.cs
+++ Skittles/CommonAsync.cs
@@ -1,7 +1,34 @@
 // ReSharper disable CheckNamespace
 
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace SkittlesPower {
     public partial class Skittles {
+        /// <summary>
+        /// Gets the most common color of an image
+        /// </summary>
+        /// <param name="source">File path or URL</param>
+        /// <param name="width">New width of the image</param>
+        /// <param name="height">New height of the image</param>
+        public static async Task<Color> CommonAsync(string source, int width = 32, int height = 32)
+            => Common(await GetImageAsync(source));
         
+        public static Color Common(Bitmap image, int width = 32, int height = 32) {
+            image = (Bitmap)image.GetThumbnailImage(width, height, () => false, IntPtr.Zero); // resizes image
+            var colors = new List<Color>();
+            // first for loop goes down the image after the second for loop has finished going left to right
+            for (var h = 0; h < image.Height; h++) 
+                for (var w = 0; w < image.Width; w++) {
+                    var pixel = image.GetPixel(w, h);
+                    if (pixel.IsEmpty || pixel.A < Tolerances.Transparency) continue;
+                    colors.Add(pixel);
+                }
+            var commons = colors.GroupBy(color => color).OrderByDescending(group => group.Count()).SelectMany(color => color).ToList();
+            return commons[0];
+        }
     }
 }
